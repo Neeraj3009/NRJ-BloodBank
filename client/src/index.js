326ba875b58a410634406e435928a66e50c89a51
Neeraj3009 <108749534+Neeraj3009@users.noreply.github.com>
2023-05-26 10:44:28 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          // colorPrimary: "#3F497F",
+          // colorBorder: "#3F497F", 
+          colorPrimary: "#820000",
+          colorBorder: "#820000",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </Provider>
+);
+
+reportWebVitals();
